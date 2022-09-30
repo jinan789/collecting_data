6924940af4d5c7022715c7513d090646bb86dca9
Jinan Jiang <jinan@berkeley.edu>
2022-09-30 10:04:06 +0800

    1

--- data_collector.py
+++ data_collector.py
@@ -12,7 +12,15 @@ with open("./all_commits_by_date/" + "all_dates" + '.json', "r") as f:
    
 dates = all_dates
 
+month = dates[0][:7]
 for date in dates:
+    cur_month = date[:7]
+    if cur_month != month:
+        month = cur_month
+        os.system("git add *")
+        os.system("git commit -m \"1\"")
+        os.system("git push")
+
     try:
         with open("./all_commits_by_date/" + date + '.json', "r") as f:
             cur_date = json.load(f)
@@ -21,8 +29,8 @@ for date in dates:
         with open("./all_commits_by_date/" + date + '_info.json', "w") as f:
             json.dump(infos, f, indent=1)
             
-        with open("completed_log.txt", "a") as f:
+        with open("./completed_log.txt", "a") as f:
             json.dump(date + "        ", f, indent=1)
     except:
-        with open("err_log.txt/", "a") as f:
+        with open("./err_log.txt/", "a") as f:
             json.dump(date + "        ", f, indent=1)
